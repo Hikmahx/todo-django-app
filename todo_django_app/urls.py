a611aa4405873efaae01c2ccca5efa7269c7962f
Hikmahx <hikmayousuph@gmail.com>
2022-01-09 00:51:57 +0100

    templateView used to add todo-react-app index.html as homepage

--- todo_django_app/urls.py
+++ todo_django_app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from todo import views
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
@@ -24,4 +25,5 @@ router.register(r'todos', views.TodoView, 'todo')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
