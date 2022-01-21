87bbc49783940cc3bd32b95ba68f129dfc8b229c
Hikmahx <hikmayousuph@gmail.com>
2022-01-21 02:28:20 +0100

    todos parsed

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ function App() {
   const totalTodo =()=>{
     let counter = 0
     setTotal(todos.length)
+    setTodos(JSON.parse(todos))
     todos.forEach(todo=>{
       if(!todo.completed){
         return counter--
